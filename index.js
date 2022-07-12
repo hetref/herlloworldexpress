2ceec6ef98d08d83959e6d8c90b5bece88687edd
hetref <shindearyan179@gmail.com>
2022-07-12 23:25:22 +0530

    some changes

--- index.js
+++ index.js
@@ -1,11 +1,26 @@
 const express = require('express')
+const mongoose = require('mongoose');
+require('dotenv/config');
+const bodyParser = require('body-parser');
 const app = express()
 const port = process.env.PORT || 3000
 
+
+// Import Routes
+const postsRoute = require('./routes/posts');
+app.use('/posts', postsRoute);
+
+// Middlewares
+app.use(bodyParser.json());
+
+// ROUTES
 app.get('/', (req, res) => {
-    res.send('Hello World!')
+    res.send('We are on Home');
 })
 
+// Connect to DB
+mongoose.connect(process.env.DB_CONNECTION_URL, () => { alert("Connected DB") })
+
 app.listen(port, () => {
-    console.log(`Example app listening on port ${port}`)
+    alert(`Example app listening on port ${port}`)
 })
